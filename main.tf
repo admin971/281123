5b6b9eedb40ff6bef683be97e8b4a429aff6b8dd
karthick <you@example.com>
2023-12-01 13:30:56 +0530

    premmite to gave accesess key password

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 terraform {
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
-  
-  # Configure the AWS Provider
-  provider "aws" {
-    region = "ap-south-1"
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_instance" "terraform" {
+  ami           = "ami-0287a05f0ef0e9d9a"
+  instance_type = "t2.micro"
+  #count =5
+   #key_name ="singapore"
+  tags = {
+    Name = "HelloWorld"
   }
-
-  resource "aws_instance" "terraform" {
-    ami           = "ami-0287a05f0ef0e9d9a"
-    instance_type = "t2.micro"
-    #count =5
-     #key_name ="singapore"
-      tags = {
-      Name = "HelloWorld"
-    }
-  }
-
-
-
+}
